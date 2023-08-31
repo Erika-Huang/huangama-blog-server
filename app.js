c0e4246b61ee28d92066e98687b7fbe02fbefcc3
Erika Huang <suiic@qq.com>
2023-08-31 16:13:03 +0800

    feat: 项目初始化

---
+++ app.js
@@ -0,0 +1,44 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views') // 能访问views下面的文件，渲染界面
+const json = require('koa-json') // 参数转为json对象
+const onerror = require('koa-onerror') // 错误监听
+const bodyparser = require('koa-bodyparser') // 把前端请求参数转换
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
